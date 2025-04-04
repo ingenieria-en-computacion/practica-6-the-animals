3f91bc38682b24883e054f73fb0a6c843eeb1a96
JuanCarlosSotoGarcia <carloscc1125@gmail.com>
2025-04-03 21:31:49 -0600

    cola_circular.h

--- include/cola_circular.h
+++ include/cola_circular.h
@@ -6,37 +6,41 @@
 #define CAPACIDAD_COLA 100
 
 typedef struct {
-    void* datos[CAPACIDAD_COLA];
+    int datos[CAPACIDAD_COLA];
     int frente;
     int final;
-} ColaGenerica;
+} ColaCircular;
 
-ColaGenerica* cola_generica_crear() {
-    ColaGenerica* cola = malloc(sizeof(ColaGenerica));
+ColaCircular* cola_crear() {
+    ColaCircular* cola = malloc(sizeof(ColaCircular));
     cola->frente = cola->final = 0;
     return cola;
 }
 
-bool cola_generica_vacia(ColaGenerica* cola) {
+bool cola_vacia(ColaCircular* cola) {
     return cola->frente == cola->final;
 }
 
-bool cola_generica_llena(ColaGenerica* cola) {
+bool cola_llena(ColaCircular* cola) {
     return (cola->final + 1) % CAPACIDAD_COLA == cola->frente;
 }
 
-bool cola_generica_encolar(ColaGenerica* cola, void* valor) {
-    if (cola_generica_llena(cola)) return false;
+bool cola_encolar(ColaCircular* cola, int valor) {
+    if (cola_llena(cola)) return false;
     cola->datos[cola->final] = valor;
     cola->final = (cola->final + 1) % CAPACIDAD_COLA;
     return true;
 }
 
-bool cola_generica_desencolar(ColaGenerica* cola, void** valor) {
-    if (cola_generica_vacia(cola)) return false;
+bool cola_desencolar(ColaCircular* cola, int* valor) {
+    if (cola_vacia(cola)) return false;
     *valor = cola->datos[cola->frente];
     cola->frente = (cola->frente + 1) % CAPACIDAD_COLA;
     return true;
+}
+
+void cola_destruir(ColaCircular* cola) {
+    free(cola);
 }
 
 void cola_generica_destruir(ColaGenerica* cola) {
