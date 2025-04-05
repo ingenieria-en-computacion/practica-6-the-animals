34cb0e2394bd9eb284d2a3b34ec2965d58b8caca
JuanCarlosSotoGarcia <carloscc1125@gmail.com>
2025-04-04 21:20:49 -0600

    cola_generica.h

--- include/cola_generica.h
+++ include/cola_generica.h
@@ -11,20 +11,26 @@ typedef struct {
     int final;
 } ColaGenerica;
 
+// Crea una nueva cola genérica
 ColaGenerica* cola_generica_crear() {
     ColaGenerica* cola = malloc(sizeof(ColaGenerica));
-    cola->frente = cola->final = 0;
+    if (cola) {
+        cola->frente = cola->final = 0;
+    }
     return cola;
 }
 
+// Verifica si la cola está vacía
 bool cola_generica_vacia(ColaGenerica* cola) {
     return cola->frente == cola->final;
 }
 
+// Verifica si la cola está llena
 bool cola_generica_llena(ColaGenerica* cola) {
     return (cola->final + 1) % CAPACIDAD_COLA == cola->frente;
 }
 
+// Encola un valor en la cola genérica
 bool cola_generica_encolar(ColaGenerica* cola, void* valor) {
     if (cola_generica_llena(cola)) return false;
     cola->datos[cola->final] = valor;
@@ -32,6 +38,7 @@ bool cola_generica_encolar(ColaGenerica* cola, void* valor) {
     return true;
 }
 
+// Desencola un valor de la cola genérica
 bool cola_generica_desencolar(ColaGenerica* cola, void** valor) {
     if (cola_generica_vacia(cola)) return false;
     *valor = cola->datos[cola->frente];
@@ -39,6 +46,7 @@ bool cola_generica_desencolar(ColaGenerica* cola, void** valor) {
     return true;
 }
 
+// Destruye la cola genérica y libera la memoria
 void cola_generica_destruir(ColaGenerica* cola) {
     free(cola);
 }
